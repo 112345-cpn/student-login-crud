df1e19f294151b058b1576dd6e7021937e833f05
MR <1063466408@qq.com>
2026-08-13 00:23:49 +0800

    二维码的接口

--- src/main/java/org/example/studentlogincrud/config/AdminAuthInterceptor.java
+++ src/main/java/org/example/studentlogincrud/config/AdminAuthInterceptor.java
@@ -2,6 +2,7 @@ package org.example.studentlogincrud.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -10,7 +11,7 @@ public class AdminAuthInterceptor implements HandlerInterceptor {
     public static final String SESSION_ADMIN_ID = "adminId";
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
         if (request.getSession(false) != null
                 && request.getSession(false).getAttribute(SESSION_ADMIN_ID) != null) {
             return true;
